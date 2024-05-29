3d6a3d75e9cd45cb83b5f85d7dd8ef69a0df4c31
Yoosup Song <Youssupsong@gmail.com>
2024-05-29 19:17:54 -0400

    Added buttons for inputs(Not functional)

--- frontend/client/src/pages/Home.tsx
+++ frontend/client/src/pages/Home.tsx
@@ -5,11 +5,19 @@ function Home() {
     window.open('http://127.0.0.1:5000/run', '_blank', 'noopener,noreferrer');
   };
 return (
-  <div className="flex justify-center">
-    <button className="btn btn-primary" onClick={runApp}>
-      Run the program
-    </button>
-  </div>
+  <div>
+      <div className="flex justify-center">
+        <button className="btn btn-primary" onClick={runApp}>
+          Run the program
+        </button>
+      </div>
+      <div className="flex justify-center">
+        <input id="variation" type="text" className="input input-bordered w-full max-w-xs mt-4"/>
+      </div>
+      <div className="flex justify-center">
+        <input id="defaultLocation" type="text" className="input input-bordered w-full max-w-xs mt-4"/>
+      </div>
+    </div>
 );
 }
 
